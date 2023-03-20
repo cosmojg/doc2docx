250c577744d02f69af8094e1956e5a1e22d33138
Cosmo Guerini <cosmo.guerini@bostonfusion.com>
2023-03-20 17:37:34 -0400

    Revert backend and importlib changes

--- doc2docx/__init__.py
+++ doc2docx/__init__.py
@@ -6,11 +6,6 @@ from pathlib import Path
 
 from tqdm.auto import tqdm
 
-try:
-    from importlib.metadata import version
-except ImportError:
-    from importlib_metadata import version
-
 __version__ = version(__package__)
 
 
